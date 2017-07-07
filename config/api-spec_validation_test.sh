23b16b02f2cb08e7e655182725ec549b05245f72
Greg Werner <werner.greg@gmail.com>
2017-07-07 09:51:27 -0400

    don't test on merge

--- config/api-spec_validation_test.sh
+++ config/api-spec_validation_test.sh
@@ -2,28 +2,33 @@
 
 testOpenApiSpecValidity() {
 
-    expectedOutput="{}"
-    expectedOutputSize=${#expectedOutput}
+    if [ "$TRAVIS_PULL_REQUEST" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    
+        expectedOutput="{}"
+        expectedOutputSize=${#expectedOutput}
 
-    # Prepares the spec URL from GitHub Pull-Request (PR)
-    specUrl="https://raw.githubusercontent.com/$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH/swagger/tbs_swagger.yml"
-    # Now prepare the open API spec file to use the online validator service.
-    validationUrl="http://online.swagger.io/validator/debug?url=$specUrl"
+        # Prepares the spec URL from GitHub Pull-Request (PR)
+        specUrl="https://raw.githubusercontent.com/$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH/swagger/tbs_swagger.yml"
+        # Now prepare the open API spec file to use the online validator service.
+        validationUrl="http://online.swagger.io/validator/debug?url=$specUrl"
 
-    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-    echo "Validating ENV Variables: Namespace=$TRAVIS_PULL_REQUEST_SLUG, Request Branch=$TRAVIS_PULL_REQUEST_BRANCH"
-    echo "OpenAPI Specification File=$validationUrl"
-    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+        echo "Validating ENV Variables: Namespace=$TRAVIS_PULL_REQUEST_SLUG, Request Branch=$TRAVIS_PULL_REQUEST_BRANCH"
+        echo "OpenAPI Specification File=$validationUrl"
+        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
-    validationOutput=$(curl $validationUrl)
-    validationOutputSize=${#validationOutput}
+        validationOutput=$(curl $validationUrl)
+        validationOutputSize=${#validationOutput}
 
-    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-    echo "Testing swagger validation - current output is: $validationOutput"
-    echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
-    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+        echo "Testing swagger validation - current output is: $validationOutput"
+        echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
+        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
-    assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
+        assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
+
+    fi
+    
 }
 
 
