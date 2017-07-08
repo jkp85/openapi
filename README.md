0282e1d15bc6a82fe1d9a600343910559ea841be
Greg Werner <werner.greg@gmail.com>
2017-07-08 14:40:57 -0400

    test invalid swagger (#49)
    * test invalid swagger
    
    * invalid yml
    
    * test from right path
    
    * test with pr only
    
    * fix
    
    * echo env type
    
    * spacing fix
    
    * build on merge
    
    * add badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/3Blades/openapi.svg?branch=master)](https://travis-ci.org/3Blades/openapi)
+
 # OpenAPI (a.k.a Swagger) Specification for 3Blades API
 
 3Blades API docs based on Swagger.
--- config/api-spec_validation_test.sh
+++ config/api-spec_validation_test.sh
@@ -3,30 +3,56 @@
 
 testOpenApiSpecValidity() {
 
-        expectedOutput="{}"
-        expectedOutputSize=${#expectedOutput}
+  expectedOutput="{}"
+  expectedOutputSize=${#expectedOutput}
 
-        # Prepares the spec URL from GitHub Pull-Request (PR)
-        specUrl="https://raw.githubusercontent.com/$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH/tbs_swagger.yml"
-        # Now prepare the open API spec file to use the online validator service.
-        validationUrl="http://online.swagger.io/validator/debug?url=$specUrl"
+  # Prepares the spec URL from GitHub Pull-Request (PR)
+  prUrl="https://raw.githubusercontent.com/$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH/tbs_swagger.yml"
+  # Now prepare the open API spec file to use the online validator service.
+  prValidationUrl="http://online.swagger.io/validator/debug?url=$prUrl"
 
-        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-        echo "Validating ENV Variables: Namespace=$TRAVIS_PULL_REQUEST_SLUG, Request Branch=$TRAVIS_PULL_REQUEST_BRANCH"
-        echo "OpenAPI Specification File=$validationUrl"
-        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+  # Prepares the spec URL from GitHub Pull-Request (PR)
+  masterUrl="https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/tbs_swagger.yml"
+  # Now prepare the open API spec file to use the online validator service.
+  masterValidationUrl="http://online.swagger.io/validator/debug?url=$masterUrl"
 
-        validationOutput=$(curl $validationUrl)
-        validationOutputSize=${#validationOutput}
+  echo "$TRAVIS_EVENT_TYPE"
 
-        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-        echo "Testing swagger validation - current output is: $validationOutput"
-        echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
-        echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+  if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
 
-        assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+    echo "Validating ENV Variables: Namespace=$TRAVIS_PULL_REQUEST_SLUG, Request Branch=$TRAVIS_PULL_REQUEST_BRANCH"
+    echo "OpenAPI Specification File=$prUrl"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
-  }
+    validationOutput=$(curl $prValidationUrl)
+    validationOutputSize=${#validationOutput}
+
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+    echo "Testing swagger validation - current output is: $validationOutput"
+    echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+    assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
+
+  else
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+    echo "Validating ENV Variables: Namespace=$TRAVIS_REPO_SLUG, Request Branch=$TRAVIS_BRANCH"
+    echo "OpenAPI Specification File=$masterUrl"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+    validationOutput=$(curl $masterValidationUrl)
+    validationOutputSize=${#validationOutput}
+
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+    echo "Testing swagger validation - current output is: $validationOutput"
+    echo "Expected valid size: $expectedOutputSize, current output: $validationOutputSize"
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+    assertEquals "Validation failed - service unavailable or error found." $expectedOutputSize $validationOutputSize
+  fi
+
+}
 
 # Execute shunit2 to run the tests (downloaded via `.travis.yaml`)
 . shunit2-2.1.6/src/shunit2
